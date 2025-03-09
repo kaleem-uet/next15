1682a574613170084764b62ab36c3737360833a8
kaleem <kaleem@archiwiz.com>
2025-03-09 15:38:03 +0500

    them added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className="font-display text-secondary  bg-primary p-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+      laudantium magni molestiae soluta vitae nam recusandae officia, accusamus
+      voluptatibus veritatis quaerat. Eaque eveniet culpa provident. Consectetur
+      placeat numquam nesciunt explicabo.
+    </div>
+  );
 }
